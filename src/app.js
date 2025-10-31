a05f6b8952a36c74765d4594c68de375c8d4c7d0
ssantv <ssantv00@gmail.com>
2025-10-31 11:18:06 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon', 'flores', 'patio'];
+let extension = ['com', 'es', 'dev', 'io', 'net'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const elemExcuses = document.getElementById("excuses")
+pronoun.forEach(pronoun => {
+  adj.forEach(adj => 
+    noun.forEach(noun => {
+      extension.forEach(extension=> {
+        let result;
+        if (noun.endsWith(extension)) {
+          result =`${pronoun}${adj}${noun.slice(0,-extension.length)}.${extension}`
+          elemExcuses.innerHTML += `<li class="list-group-item bg-warning"> ${result} </li>`
+        } else {
+          result =`${pronoun}${adj}${noun}.${extension}`
+          elemExcuses.innerHTML += `<li class="list-group-item"> ${result} </li>`}
+      })
+    })
+  )
+})
+console.log(elemExcuses)
